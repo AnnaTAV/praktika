bd56def5c0aba0dd2082d3e705f8159e29779a65
Anna <trishechkinaana@gmail.com>
2022-07-14 19:31:42 +0300

    Added Home work_5 task 10

---
+++ Home work_5/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int num = new Random().Next(100,1000);
+int result = RemoveSecond(num);
+Console.WriteLine($"{num} -> {result}");
+
+int RemoveSecond(int num)
+{
+    int first = num % 100;
+    int second = num / 10;// int second = num * 10;
+    return second % 10;// return first * 10 + third;
+}
