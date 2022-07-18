2ada3d25da2893979c35a4e3e73a7bd984016e50
Anna <trishechkinaana@gmail.com>
2022-07-18 18:48:09 +0300

    Added task 19

---
+++ Home work_9 Task 19/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 19
+
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+// 14212 -> нет
+
+// 12821 -> да
+
+// 23432 -> да
+
+Console.WriteLine("Введите значение: ");
+string num = Console.ReadLine();
+void PalindromeNumber(string num)
+{
+    if (num[0] == num[4] || num[1] == num[3])
+    {
+        Console.WriteLine($"Значение: {num} - является палиндромом.");
+    }
+    else Console.WriteLine($"Значение: {num} - не является палиндромом.");
+}
+if (num.Length == 5)
+{
+    PalindromeNumber(num);
+}
+else Console.WriteLine($"Введите правильное значение");
