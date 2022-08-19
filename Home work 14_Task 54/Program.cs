cce612cb6dc2a0a734d01c6624cb444593b1d464
Anna <trishechkinaana@gmail.com>
2022-08-19 20:11:03 +0300

    added task 54

--- Home work 14_Task 54/Program.cs
+++ Home work 14_Task 54/Program.cs
@@ -40,11 +40,11 @@ void OrderedArray(int[,] matrix)
 {
   for (int i = 0; i < matrix.GetLength(0); i++)
   {
-    for (int j = 0; j < matrix.GetLength(1)-1; j++)
+    for (int j = 0; j < matrix.GetLength(1) - 1; j++)
     {
       for (int f = 0; f < matrix.GetLength(1) - 1; f++)
       {
-        if (matrix[i, f] > matrix[i, f + 1])
+        if (matrix[i, f] < matrix[i, f + 1])
         {
           int num = 0;
           num = matrix[i, f];
